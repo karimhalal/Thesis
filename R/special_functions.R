94b548612ed7a306d00dd51d03edeb2c3683e757
Karim <ahala028@uottawa.ca>
2025-07-09 13:04:55 -0400

    added smoking functions to special functions file

--- R/special_functions.R
+++ R/special_functions.R
@@ -1,18 +1,22 @@
-#Full list of functions that the derived
+#list of functions to derive variables of interest
+adl_fun <- cchsflow::adl_fun
+adl_score_5_fun <- cchsflow::adl_score_5_fun
+binge_drinker_fun <- cchsflow::binge_drinker_fun
 if_else2 <- cchsflow::if_else2
 low_drink_short_fun <- cchsflow::low_drink_short_fun
 low_drink_long_fun <- cchsflow::low_drink_long_fun
 age_cat_fun <- cchsflow::age_cat_fun
-energy_exp_fun <- cchsflow::energy_exp_fun
-bmi_fun <- cchsflow::bmi_fun
-bmi_fun_cat <- cchsflow::bmi_fun_cat
-immigration_fun <- cchsflow::immigration_fun
 low_drink_score_fun <- cchsflow::low_drink_score_fun
 low_drink_score_fun1 <- cchsflow::low_drink_score_fun1
-pack_years_fun <- cchsflow::pack_years_fun
-pack_years_fun_cat <- cchsflow::pack_years_fun_cat
-pct_time_fun <- cchsflow::pct_time_fun
-pct_time_fun_cat<- cchsflow::pct_time_fun_cat
 SMKG040_fun <- cchsflow::SMKG040_fun
 time_quit_smoking_fun <- cchsflow::time_quit_smoking_fun
 smoke_simple_fun <- cchsflow::smoke_simple_fun
+surveycycle_fun<-cchsflow::surveycycle_fun
+
+
+#change name of the survey cycle to simplify it
+surveycycle_fun<- function (data_name){
+switch(data_name, cchs2013_2014= {return("2013-2014")},
+cchs2015_2016= {return("2015-2016")},
+cchs2017_2018= {return("2017-2018")},)
+}
