fe5d2ed6745f6b64a4fa333169a14e06902508df
Karim <ahala028@uottawa.ca>
2025-11-17 15:54:46 -0500

    Refactor DIN_list sheets
    Edit format error in DIN sheets causing excess rows

--- R/conversion_factor_assignment.R
+++ R/conversion_factor_assignment.R
@@ -7,13 +7,22 @@ din_list<-read.csv("/Users/karimhalal/Desktop/The worlds greatest thesis/Thesis/
 library(dplyr)
 library(stringr)
 
-#COLUMNS PROCESSING (ADDING LEADING 0S AND CORRECTING SPELLING MISTKAKES)
+#get rid of empty rows (artifact from CSV export)
+din_list<-din_list%>%
+  filter(!is.na(DIN.PIN))
+
+write.csv(din_list,"worksheets/DIN _list.csv",row.names=FALSE)
+
+#write edits to new csv file
+# #COLUMNS PROCESSING (ADDING LEADING 0S AND CORRECTING SPELLING MISTKAKES)
 din_list<-din_list%>%
   mutate(Active.Ingredients=tolower(Active.Ingredients))%>%
   mutate(Active.Ingredient.Class.and.Use=tolower(Active.Ingredient.Class.and.Use))%>%
+  mutate(Dosage.Form=tolower(Dosage.Form))%>%
   mutate(DIN.PIN=str_pad(DIN.PIN, width = 8, side = "left", pad = "0"))%>%
   mutate(Active.Ingredients=case_when(
    Active.Ingredients=="hydrormorphone hydrochloride"~"hydromorphone hydrochloride",
+   Active.Ingredients=="hydromophone hydrochloride"~"hydromorphone hydrochloride",
     Active.Ingredients=="butalbital + acetylsalicylic acid + caffeine"~"butalbital + acetylsalicylic acid + caffeine+codeine",
     TRUE~Active.Ingredients
   ))
@@ -33,12 +42,34 @@ din_list<-din_list%>%
     grepl("opioid", Active.Ingredient.Class.and.Use) & grepl("tapentadol", Active.Ingredients) ~ 0.3,
     grepl("opioid", Active.Ingredient.Class.and.Use) & grepl("oxymorphone", Active.Ingredients) ~ 3,
     grepl("opioid", Active.Ingredient.Class.and.Use) & grepl("methadone", Active.Ingredients) ~ 4.7,
-    grepl("opioid", Active.Ingredient.Class.and.Use) & grepl("fentanyl", Active.Ingredients)& grepl("Trans Patch", Dosage.Form) ~ 3.8,
+    grepl("opioid", Active.Ingredient.Class.and.Use) & grepl("buprenorphine", Active.Ingredients) & grepl("tab", Dosage.Form)~ 38.8,
+    grepl("opioid", Active.Ingredient.Class.and.Use) & grepl("buprenorphine", Active.Ingredients) & grepl("trans patch", Dosage.Form)~ 2.2,
+    grepl("opioid", Active.Ingredient.Class.and.Use) & grepl("buprenorphine", Active.Ingredients) & grepl("film", Dosage.Form)~ 0.039,
+    grepl("opioid", Active.Ingredient.Class.and.Use) & grepl("fentanyl", Active.Ingredients)& grepl("trans patch", Dosage.Form) ~ 4.2,
+    grepl("opioid", Active.Ingredient.Class.and.Use) & grepl("fentanyl", Active.Ingredients)& grepl("buccal soluble fil|tab", Dosage.Form) ~ 0.13,
     TRUE ~ NA_real_
   ))%>%
   #Exclude medications for which the strength of evidence for conversion is weak. This includes butorphanol and diphenoxylate (methadone and buprenorphine)
   filter(!grepl("diphenoxylate", Active.Ingredients))%>%
-  filter(!grepl("butorphanol", Active.Ingredients))
-  #filter out the injectible
+  filter(!grepl("butorphanol", Active.Ingredients))%>%
+  #filter out the injectible drugs, these are short acting drugs usually used in clinical settings
+  filter(!grepl("inj", Dosage.Form))%>%
 
-#check on the missing conversion factors for opioids
+#assign diazepam equivalents conversion factor to relevent BZD
+  mutate(conversion_factor=case_when(
+    grepl("bzd", Active.Ingredient.Class.and.Use)&grepl("alprazolam",Active.Ingredients)&grepl("cap|tab",Dosage.Form)~10,
+    grepl("bzd", Active.Ingredient.Class.and.Use)&grepl("chlordiazepoxide",Active.Ingredients)&grepl("cap|tab",Dosage.Form)~0.4,
+    grepl("bzd", Active.Ingredient.Class.and.Use)&grepl("clobazam",Active.Ingredients)&grepl("cap|tab",Dosage.Form)~0.5,
+    grepl("bzd", Active.Ingredient.Class.and.Use)&grepl("clonazepam",Active.Ingredients)&grepl("cap|tab",Dosage.Form)~10,
+    grepl("bzd", Active.Ingredient.Class.and.Use)&grepl("clorazepate",Active.Ingredients)&grepl("cap|tab",Dosage.Form)~0.67,
+    grepl("bzd", Active.Ingredient.Class.and.Use)&grepl("diazepam",Active.Ingredients)&grepl("cap|tab",Dosage.Form)~1,
+    grepl("bzd", Active.Ingredient.Class.and.Use)&grepl("flurazepam",Active.Ingredients)&grepl("cap|tab",Dosage.Form)~0.33,
+    grepl("bzd", Active.Ingredient.Class.and.Use)&grepl("lorazepam",Active.Ingredients)&grepl("cap|tab",Dosage.Form)~5,
+    grepl("bzd", Active.Ingredient.Class.and.Use)&grepl("oxazepam",Active.Ingredients)&grepl("cap|tab",Dosage.Form)~0.33,
+    grepl("bzd", Active.Ingredient.Class.and.Use)&grepl("temazepam",Active.Ingredients)&grepl("cap|tab",Dosage.Form)~0.5,
+    grepl("bzd", Active.Ingredient.Class.and.Use)&grepl("triazolam",Active.Ingredients)&grepl("cap|tab",Dosage.Form)~20,
+    grepl("bzd", Active.Ingredient.Class.and.Use)&grepl("nitrazepam",Active.Ingredients)&grepl("cap|tab",Dosage.Form)~1,
+    TRUE~conversion_factor
+  ))
+
+write.csv(din_list, "worksheets/DIN_list2.csv", row.names=FALSE)
